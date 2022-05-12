dc6520fe8f00ed9a40456fad5704590dd713b52c
Kc <kc@fog.moe>
2022-05-12 13:57:42 +0800

    Update AutoChangeKey.py

--- AutoChangeKey.py
+++ AutoChangeKey.py
@@ -2,13 +2,13 @@ import base64
 from dataclasses import replace
 import time
 
-#FOGMOE-2022/03/27-Sawe25asg@
+#FOGMOE-2022-03-27-Sawe25asg@
 def ChangeKeyDate():
     with open("Steam-DLC-Tool (Server)\SteamDLCTool\KeyToday.youhavetocrackthisfile","r",encoding = "utf-8") as file:
         oldKey = base64.b64decode(file.read()).decode("utf-8")
         file.close()
     with open("Steam-DLC-Tool (Server)\SteamDLCTool\KeyToday.youhavetocrackthisfile","w",encoding = "utf-8") as file:    
-        date = time.strftime("%Y/%m/%d", time.localtime())
+        date = time.strftime("%Y-%m-%d", time.localtime())
         oldDate = oldKey[7:17]
         newKey = oldKey.replace(oldDate,date).encode("utf-8")
         newContent = str(base64.b64encode(newKey),'UTF-8')
